81dccaab01922f1ded198f8db8fd36ef40b439bb
NAJIMx0 <sotanajim007@gmail.com>
2026-01-06 12:28:18 +0100

    reentraReentrantLock processPayment DONE !

--- src/main/java/com/najim/synchronization/PaymentProcessor.java
+++ src/main/java/com/najim/synchronization/PaymentProcessor.java
@@ -6,15 +6,27 @@ import com.najim.model.Payment;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PaymentProcessor {
-    private ReentrantLock lock = new ReentrantLock();
+    private static ReentrantLock lock = new ReentrantLock();
 
-public static Payment processPayment(LocalDateTime entry , LocalDateTime exit ,String pymethod, Integer tkId) throws SQLException {
-        double fee = PaymentService.calculateFee(entry,exit);
-        Payment py = new Payment(fee , exit ,pymethod, tkId);
-        PaymentDAO.savePayment(py);
-        return py;
+public static Payment processPayment(LocalDateTime entry , LocalDateTime exit ,String pymethod, Integer tkId) throws Exception {
+    if(lock.tryLock(5, TimeUnit.SECONDS)){
+        try {
+            double fee = PaymentService.calculateFee(entry,exit);
+            Payment py = new Payment(fee , exit ,pymethod, tkId);
+            PaymentDAO.savePayment(py);
+            return py;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }finally {
+            lock.unlock();
+        }
+    }else{
+        throw new Exception("Payment processing timeout - too busy!");
     }
+    return null;
+}
 }
