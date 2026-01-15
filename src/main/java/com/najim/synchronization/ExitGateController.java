e57c98379fe599a4f0adc9be7bb0389a5a9b93c6
NAJIMx0 <sotanajim007@gmail.com>
2026-01-15 19:13:31 +0100

    udpadet in exitGateController and ExitCarTask

--- src/main/java/com/najim/synchronization/ExitGateController.java
+++ src/main/java/com/najim/synchronization/ExitGateController.java
@@ -1,12 +1,15 @@
 package com.najim.synchronization;
 
+import com.najim.Service.ParkingService;
+import com.najim.model.Payment;
+
 import java.util.concurrent.Semaphore;
 
 public class ExitGateController {
 
     private Semaphore exitGates = new Semaphore(2);
 
-    public void exitThroughGate(String plateNumber) throws InterruptedException {
+    public void exitThroughGate(String plateNumber,String paymentMethod) throws Exception {
 
         System.out.println("Car " + plateNumber + " waiting for exit gate...");
 
@@ -14,7 +17,13 @@ public class ExitGateController {
 
         try {
             System.out.println("Car " + plateNumber + " exiting through gate...");
-            Thread.sleep(2000);
+            Payment py = ParkingService.exitCar(plateNumber, paymentMethod);
+            if (py != null) {
+                System.out.println("Car " + plateNumber + " EXITED! Fee: " +
+                        String.format("%.2f", py.getAmount())+" DH");
+            } else {
+                System.out.println("Car " + plateNumber + " exit FAILED");
+            }
             System.out.println("Car " + plateNumber + " passed through exit gate!");
 
         } finally {
--- src/main/java/com/najim/threads/CarExitTask.java
+++ src/main/java/com/najim/threads/CarExitTask.java
@@ -19,16 +19,9 @@ public class CarExitTask implements Runnable {
     public void run() {
         try {
             System.out.println("Car "+platnumber+" is trying to EXIT...");
-            Payment py = ParkingService.exitCar(platnumber, paymentMethod);
 
-            if (py != null) {
-                System.out.println("Car " + platnumber + " EXITED! Fee: " +
-                        String.format("%.2f", py.getAmount())+" DH");
+            exitGateController.exitThroughGate(platnumber,paymentMethod);
 
-                exitGateController.exitThroughGate(platnumber);
-            } else {
-                System.out.println("Car " + platnumber + " exit FAILED");
-            }
 //            System.out.println("Car " + platnumber + " EXITED! Fee: " + py.getAmount()+" DH");
         }catch (Exception e){
             System.out.println("Car Exit Task Error"+e.getMessage());
